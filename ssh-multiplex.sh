7ddf617312860d66a22f0531d56f344d96c86206
Christian Becker <mail@christianbecker.name>
2023-04-25 22:31:37 +0200

    nested tmux support

--- ssh-multiplex.sh
+++ ssh-multiplex.sh
@@ -10,21 +10,45 @@ if [[ $* == "-h" || $* == "--help" || $* == "" || ${*#-} != "$*" ]]; then
 fi 
 
 
-# session name
-SESSION="SSH-MULTIPLEX-$$"
+# set tmux id
+ID="SSH-MULTIPLEX"
 
-# create first ssh session, detach from it and delete host from list
-tmux new-session -s "$SESSION" -d "ssh $1 ; read -n 1 -p \"Connection to $1 - Press any key to close...\""
-shift
+# check if already inside a tmux session - then create a new window - else create a session
+if [ "$TMUX" ]; then
+    # session name - from existing tmux session
+    SESSION="$(tmux display-message -p '#S')"
 
-# connect the remaining hosts inside the tmux session and select tiled layout
+    # create first ssh session in a new window inside the tmux session and delete host from list
+    i=1
+    tmux new-window -t "$SESSION" -n "$ID" -a "ssh $1 ; read -n 1 -p \"Connection to $1 - Press any key to close...\""
+    shift
+
+else
+    # session name - for a new tmux session
+    SESSION="$ID-$$"
+
+    # create first ssh session, detach from it and delete host from list
+    i=1
+    tmux new-session -s "$SESSION" -n "$ID" -d "ssh $1 ; read -n 1 -p \"Connection to $1 - Press any key to close...\""
+    shift
+
+    # disable the tmux status bar
+    tmux set-option -t "$SESSION" -n "$ID" status off
+
+fi
+
+
+# connect the remaining hosts via ssh inside the tmux session and select tiled layout
 for i in "$@" ; do
-    tmux split-window -t "$SESSION" "ssh $i ; read -n 1 -p \"Connection to $i - Press any key to close...\""
-    tmux select-layout -t "$SESSION" tiled
+    tmux split-window -t "$SESSION:$ID" "ssh $i ; read -n 1 -p \"Connection to $i - Press any key to close...\""
+    tmux select-layout -t "$SESSION:$ID" tiled
 done
 
-# synchronize the input, disable status bar and attach to the tmux session
-tmux set-window-option -t "$SESSION" synchronize-panes on
-tmux set-option -t "$SESSION" status off
-tmux attach-session -t "$SESSION"
+# synchronize the input
+tmux set-window-option -t "$SESSION:$ID" synchronize-panes on
+
+# attach to the newly created tmux session if not already inside
+if [ -z "$TMUX" ]; then
+    tmux attach-session -t "$SESSION"
+fi
 
