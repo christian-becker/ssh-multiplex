3085e47fbb1b0ab6feff3f78669ef59efdb97f0f
Christian Becker <mail@christianbecker.name>
2023-04-26 21:30:05 +0200

    variable for ssh command / optimized comments

--- ssh-multiplex.sh
+++ ssh-multiplex.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Multiplex SSH connections to multiple hosts with simultanious input.
+# 2023 - Christian Becker - mail@christianbecker.name
 
 # help / usage - display message if requested, no host or an unsupported option is provided
 if [[ $* == "-h" || $* == "--help" || $* == "" || ${*#-} != "$*" ]]; then 
@@ -13,41 +14,45 @@ fi
 # set tmux id
 ID="SSH-MULTIPLEX"
 
-# check if already inside a tmux session - then create a new window - else create a session
+# set ssh command
+SSHCMD="ssh _DESTINATION_ ; read -n 1 -p \"Connection to _DESTINATION_ - Press any key to close...\""
+
+
+# check if already inside a tmux session - then just create a new window in it
 if [ "$TMUX" ]; then
     # session name - from existing tmux session
     SESSION="$(tmux display-message -p '#S')"
 
     # create first ssh session in a new window inside the tmux session and delete host from list
-    i=1
-    tmux new-window -t "$SESSION" -n "$ID" -a "ssh $1 ; read -n 1 -p \"Connection to $1 - Press any key to close...\""
+    tmux new-window -t "$SESSION" -n "$ID" -a "${SSHCMD//_DESTINATION_/$1}"
     shift
 
+# if not already inside tmux - create a new session
 else
     # session name - for a new tmux session
     SESSION="$ID-$$"
 
-    # create first ssh session, detach from it and delete host from list
-    i=1
-    tmux new-session -s "$SESSION" -n "$ID" -d "ssh $1 ; read -n 1 -p \"Connection to $1 - Press any key to close...\""
+    # create first ssh session in a new tmux session, detach from it and delete host from list
+    tmux new-session -s "$SESSION" -n "$ID" -d "${SSHCMD//_DESTINATION_/$1}"
     shift
 
     # disable the tmux status bar
-    tmux set-option -t "$SESSION" -n "$ID" status off
+    tmux set-option -t "$SESSION:$ID" status off
 
 fi
 
 
-# connect the remaining hosts via ssh inside the tmux session and select tiled layout
+# connect to the remaining hosts via ssh inside the tmux session and select tiled layout
 for i in "$@" ; do
-    tmux split-window -t "$SESSION:$ID" "ssh $i ; read -n 1 -p \"Connection to $i - Press any key to close...\""
+    tmux split-window -t "$SESSION:$ID" "${SSHCMD//_DESTINATION_/$i}"
     tmux select-layout -t "$SESSION:$ID" tiled
 done
 
-# synchronize the input
+
+# synchronize the input between all tmux panes
 tmux set-window-option -t "$SESSION:$ID" synchronize-panes on
 
-# attach to the newly created tmux session if not already inside
+# attach to the newly created tmux session, if not already inside
 if [ -z "$TMUX" ]; then
     tmux attach-session -t "$SESSION"
 fi
